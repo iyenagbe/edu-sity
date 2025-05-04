9c3fe832a59c771f76a5fc60a3ca564005cba4f1
IYENAGBE RAYMOND <iyenagbe>
2025-05-05 00:03:19 +0100

    edusity

--- src/components/programs/programs.jsx
+++ src/components/programs/programs.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './programs.css'
-import program_1 from '../../assets/program_1.png'
-import program_2 from '../../assets/program_2.png'
-import program_3 from '../../assets/program_3.png'
-import program_icon_1 from '../../assets/program_1.png'
-import program_icon_2 from '../../assets/program_2.png'
-import program_icon_3 from '../../assets/program_3.png'
+// import program_1 from '../../assets/program-1.png'
+// import program_2 from '../../assets/program-2.png'
+// import program_3 from '../../assets/program-3.png'
+// import program_icon_1 from '../../assets/program-icon-1.png'
+// import program_icon_2 from '../../assets/program-icon-2.png'
+// import program_icon_3 from '../../assets/program-icon-3.png'
 
 export const programs = () => {
   return (
