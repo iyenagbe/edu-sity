72a178bdcafe360f0504ddab08328ccb6fbb860d
IYENAGBE RAYMOND <iyenagbe>
2025-05-28 23:14:50 +0100

    edu sity

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,18 +14,25 @@ export const Navbar = () => {
 
   }, []);
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className= {`container ${sticky ? 'dark-navbar' : ''}`}>
 
         <img src="image/logo.png" alt="" className='logo'/>
-        <ul>
+        
+        <ul className={mobileMenu ? "" : "hide-moblie-menu"}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-            <li><Link to='about' smooth={true} offset={0} duration={500}></Link>About</li>
-            <li><Link to='campus' smooth={true} offset={0} duration={500}></Link>Campus</li>
-            <li><Link to='programs' smooth={true} offset={0} duration={500}></Link>Programs</li>
-            <li><Link to='testimonials' smooth={true} offset={0} duration={500}></Link>Testimonials</li>
-            <li><Link to='contact' smooth={true} offset={0} duration={500} className="btn">Contact Us</Link></li>
+            <li><Link to='programs' smooth={true} offset={-260} duration={500}>Programs</Link></li>
+            <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
+            <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+            <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+            <li><Link to='contact' smooth={true} offset={-260} duration={500} className="btn">Contact Us</Link></li>
         </ul>
+        <img src="image/menu-icon.png" alt="" className='menu-icon' onClick={toggleMenu} />
     </nav>
   )
 }
