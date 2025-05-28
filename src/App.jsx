1a8d3a7efaddf5acea0eb93439f9912dfa981d35
IYENAGBE RAYMOND <iyenagbe>
2025-05-28 15:46:25 +0100

    edu sity

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 
 export const App = () => {
@@ -33,6 +34,8 @@ export const App = () => {
       
       <Title subTitle='Contact Us' title='Get in Touch' />
       <Contact />
+
+      <Footer />
       
       </div>
     </div>
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <p>&copy; 2025 Iyenagbe University of Science and Technology. All rights reserved.</p>
+      
+    </div>
+  )
+}
+
+export default Footer
