cc5a724176b835405a15f62b522e171f4f6f6e79
IYENAGBE RAYMOND <iyenagbe>
2025-05-30 23:56:46 +0100

    cool

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { use, useRef } from 'react'
 import './VideoPlayer.css';
 
 const VideoPlayer = ({playerState, setPlayerState}) => {
+
+  const player = useRef(null);
+
+  const closePlayer = (e) => {
+    if (e.target === player.current){
+      setPlayerState(false);
+    }
+    
+    
+  }
+
+
+
   return (
-    <div className={`video-player ${playerState ? '' : 'hide'}`}>
+    <div className={`video-player ${playerState ? '' : 'hide'}`} ref={player} onClick={closePlayer} >
         <video src="image/videoplayer.mp4" autoPlay muted controls />
       
     </div>
